37f02689a3d8cd192e0b8634ca322a944ed139d8
Leon <bizindaua@gmail.com>
2021-08-13 00:53:23 +0300

    -d03

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,5 +5,6 @@ public class Main {
         System.out.println("With or without u");
         System.out.println("With or without u");
         System.out.println("With or without u");
+        System.out.println("With or without u");
     }
 }
